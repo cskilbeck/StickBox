1f6301bc61ffa13ae1ff3a06a576167832bb40f5
chs <github@charlie.skilbeck.com>
2019-09-07 20:49:29 +0100

    tidy up Blocks.Flags accessors

--- Assets/Block.cs
+++ Assets/Block.cs
@@ -12,41 +12,58 @@ public class Block
     {
         none = 0,
         is_stuck = 1,
-        was_visited = 2 // more for other crap like hovered, can_move, can_be_selected etc
+        was_visited = 2, // more for other crap like hovered, can_move, can_be_selected etc
+        all = 0xff
     };
 
     public GameObject quad;
     public Vector2Int position;
     public Flags flags = Flags.none;
 
+    void set(Flags f)
+    {
+        flags |= f;
+    }
+
+    void clr(Flags f)
+    {
+        flags &= ~f;
+    }
+
     public bool stuck
     {
-        get { return (flags & Flags.is_stuck) != 0; }
+        get
+        {
+            return (flags & Flags.is_stuck) != 0;
+        }
         set
         {
             if (value)
             {
-                flags |= Flags.is_stuck;
+                set(Flags.is_stuck);
             }
             else
             {
-                flags &= ~Flags.is_stuck;
+                clr(Flags.is_stuck);
             }
         }
     }
 
     public bool visited
     {
-        get { return (flags & Flags.was_visited) != 0; }
+        get
+        {
+            return (flags & Flags.was_visited) != 0;
+        }
         set
         {
             if (value)
             {
-                flags |= Flags.was_visited;
+                set(Flags.was_visited);
             }
             else
             {
-                flags &= ~Flags.was_visited;
+                clr(Flags.was_visited);
             }
         }
     }
