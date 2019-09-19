4ffe419ce08de388bfe5fd75eb19c5511c7fda13
Charlie Skilbeck <github@charlie.skilbeck.com>
2019-09-19 07:57:03 +0100

    add board_coordinate for Game

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -43,29 +43,69 @@ public class Game : MonoBehaviour
 
     //////////////////////////////////////////////////////////////////////
 
-    public GameObject create_block_object(Color color, float offset = 0)
+    static readonly int max_grid_width = 16;
+    static readonly int max_grid_height = 16;
+    static Vector3 block_scale;
+
+    //////////////////////////////////////////////////////////////////////
+
+    public GameObject create_block_object(Color color)
     {
         GameObject quad_object = GameObject.CreatePrimitive(PrimitiveType.Cube);
         quad_object.GetComponent<Renderer>().material.shader = block_shader;
-        Main.set_color(quad_object, new Color(1,0,0));
-        //Main.set_transparent(quad_object);
+        Main.set_color(quad_object, color);
         return quad_object;
     }
+
+    Vector3 board_coordinate(Vec2i pos)
+    {
+        return new Vector3((pos.x - 7.5f) * block_scale.x, (pos.y - 7.5f) * block_scale.x, -block_scale.z / 2);
+    }
+
+    public GameObject create_block(Vec2i pos)
+    {
+        GameObject o = create_block_object(new Color(1, 0, 0));
+        o.transform.localScale = block_scale;
+        o.transform.SetParent(front_face.transform, false);
+        o.transform.localPosition = board_coordinate(pos);
+        return o;
+    }
+
+    GameObject s;
+    Vector3 target;
+    float start_time;
 
     // Start is called before the first frame update
     void Start()
     {
-        GameObject o = create_block_object(Color.yellow);
-        o.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
-        o.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
-        o.transform.SetParent(front_face.transform, false);
+        Bounds face_size = front_face.GetComponent<Renderer>().bounds;
+        float face_width = face_size.size.x;
+        float face_height = face_size.size.y;
 
-        Debug.Log(front_face.GetComponent<Renderer>().bounds);
+        float x_scale = face_width / (max_grid_width + 1);
+        float y_scale = face_height / (max_grid_height + 1);
+        float z_scale = Mathf.Max(x_scale, y_scale);
+        block_scale = new Vector3(x_scale, y_scale, z_scale);
+
+        s = create_block(new Vec2i(0, 0));
+        create_block(new Vec2i(7, 0));
+        create_block(new Vec2i(8, 0));
+        create_block(new Vec2i(9, 0));
+        create_block(new Vec2i(7, 1));
+        create_block(new Vec2i(7, 2));
+        create_block(new Vec2i(15, 0));
+        create_block(new Vec2i(15, 15));
+
+        target = board_coordinate(new Vec2i(0, 15));
+        start_time = Time.realtimeSinceStartup;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        Vector3 a = board_coordinate(new Vec2i(0, 0));
+        Vector3 b = board_coordinate(new Vec2i(0, 15));
+        float t = Mathf.Min(4, Time.realtimeSinceStartup - start_time);
+        s.transform.localPosition = Vector3.Lerp(a, b, t / 4);
     }
 }
