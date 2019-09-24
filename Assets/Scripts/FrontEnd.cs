5b82aeefe315eae5f82717bf448a9215ac6eca85
Charlie Skilbeck <github@charlie.skilbeck.com>
2019-09-24 08:04:52 +0100

    add level unlock by tens

--- Assets/Scripts/FrontEnd.cs
+++ Assets/Scripts/FrontEnd.cs
@@ -9,6 +9,12 @@ public class FrontEnd : MonoBehaviour
 {
     public GameObject button_panel;
     public Button button_prefab;
+    public bool play_any_level;
+
+    int highest_completed_level;
+    int max_level_enabled = 10;
+
+    int completion_mask;
 
     void clicked(int index)
     {
@@ -21,17 +27,44 @@ public class FrontEnd : MonoBehaviour
     {
         Button b = Instantiate(button_prefab);
 
-        Color button_color = Color.grey;
+        // if last 10 levels complete and we're on a multiple of 10, enable the next 10 levels
+        int ten_last_mask = (1 << 10) - 1;
+        if ((index % 10) == 0 && (completion_mask & ten_last_mask) == ten_last_mask)
+        {
+            max_level_enabled = index + 10;
+        }
+
+        // low 10 bits of completion_mask are last 10 levels completed status
+        completion_mask <<= 1;
+        if (Statics.level_complete[index])
+        {
+            completion_mask |= 1;
+        }
+
+#if UNITY_EDITOR
+        if (play_any_level)
+        {
+            max_level_enabled = 100;
+        }
+#endif
+
+        Color button_color = Color.white;
         Color text_color = Color.black;
-        if (File.load_level(index))
+
+        if (File.load_level(index) && index < max_level_enabled)
         {
             b.onClick.AddListener(() => { clicked(index); });
-            button_color = Color.white;
         }
+        else
+        {
+            button_color = Color.grey;
+        }
+
         if (Statics.level_complete[index])
         {
             button_color = Color.yellow;
         }
+
         if (Statics.level_cheat[index])
         {
             text_color = Color.magenta;
@@ -53,7 +86,7 @@ public class FrontEnd : MonoBehaviour
         float h = 72;
         float xo = w * 5 - w / 2;
         float yo = h * 5 - h / 2;
-        for (int y=0; y<10; ++y)
+        for (int y = 0; y < 10; ++y)
         {
             for (int x = 0; x < 10; ++x)
             {
